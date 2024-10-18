ad5f2c3e89585d00f5de2e1f0bd06cb59a94a225
Klever Gopar <20468297+klever12@users.noreply.github.com>
2024-10-18 04:38:06 +0000

    implemented payment path and erb

--- app.rb
+++ app.rb
@@ -8,11 +8,11 @@ get("/") do
   "
 end
 
-get("/square/new") do 
+get("/square/new") do
   erb(:new_square_calc)
 end
 
-get('/square/results') do
+get("/square/results") do
   @the_num = params.fetch("users_number").to_f
 
   @the_result = @the_num ** 2
@@ -20,14 +20,34 @@ get('/square/results') do
   erb(:square_results)
 end
 
-get("/square_root/new") do 
+get("/square_root/new") do
   erb(:new_square_root_calc)
 end
 
-get('/square_root/results') do
+get("/square_root/results") do
   @the_num = params.fetch("users_number").to_f
 
   @the_result = @the_num ** 0.5
 
   erb(:square_root_results)
+end
+
+get("/payment/new") do
+  erb(:payment_calc)
+end
+
+get("/payment/results") do
+  @the_apr = params.fetch("user_apr").to_f
+  @the_years = params.fetch("user_years").to_i
+  @the_pv = params.fetch("user_pv").to_f
+
+  handled_apr = (@the_apr / 100) / 12
+  handled_years = @the_years * 12
+
+  numerator = handled_apr * @the_pv
+  denominator = 1 - ((1 + handled_apr) ** -handled_years)
+ 
+  @the_result = numerator / denominator
+
+  erb(:payment_result)
 end
